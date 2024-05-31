08503c77dea6237bef58aa248a12e68c30ce4aa8
justnotiyan <muhammadfitrian0712@gmail.com>
2024-06-01 02:57:49 +0700

    adding animation scaling on hover and change image to rainbow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
 	content: ["*.html"],
 	theme: {
-		extend: {},
+		fontFamily: {
+			patrick: ["Patrick Hand", "cursive"],
+		},
+		extend: {
+			colors: {
+				brand: {
+					black: {
+						100: "#374151",
+						200: "#111827",
+						300: "#4B5563",
+					},
+				},
+			},
+		},
 	},
 	plugins: [],
 };
